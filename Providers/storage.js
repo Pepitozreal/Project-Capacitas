5a939e2fed2c8f1e712b269a15d328ef92b363aa
Benjamin <benmid@student.chalmers.se>
2018-07-26 20:06:40 +0200

    Added storage provider

---
+++ Providers/storage.js
@@ -0,0 +1,15 @@
+import {AsyncStorage} from 'react-native';
+
+const Storage = {
+    saveImage: (image) => {
+        // Code to save image
+    },
+    loadImage: (key) => {
+        // Code to load image
+    },
+    loadAllImages: () => {
+        // Loads all images from storage
+    }
+}
+
+export default Storage;
