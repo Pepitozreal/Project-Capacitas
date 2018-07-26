e6a34d724440ff5dd0678c2a5fea4cd04c341b21
Benjamin <benmid@student.chalmers.se>
2018-07-26 21:28:55 +0200

    Camera catch

--- Providers/storage.js
+++ Providers/storage.js
@@ -5,6 +5,8 @@ import {AsyncStorage} from 'react-native';
 export default Storage = {
     saveImage: (image) => {
         AsyncStorage.setItem("Image", image)
+        .then(() => alert("saved!"))
+        .catch((e) => alert(e))
     },
     loadImage: (key) => {
        AsyncStorage.getItem("Image", (result) => {
